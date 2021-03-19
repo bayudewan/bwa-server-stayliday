c919614ab0c1c5db8b50815767167ad343a9af58
bayudewan <bdewandesign@gmail.com>
2021-03-19 11:31:46 +0700

    cors install

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 // import mongoose
 // local mongodb://127.0.0.1:27017/db_stayliday
 const mongoose = require('mongoose');
@@ -24,6 +25,7 @@ const apiRouter = require('./routes/api');
 
 var app = express();
 
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
